6372fecf262a762be593105c950f7b94abe7d097
beyondsundae <p00000rice@gmail.com>
2020-06-20 13:46:20 +0700

    Updates

---
+++ precache-manifest.8abbc90e7cf7920cdc64d8cfea8cabea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9307154373f76172e4f0022d1caf0b63",
+    "url": "/dialog-eiei/index.html"
+  },
+  {
+    "revision": "109bc450ff9cc3e67023",
+    "url": "/dialog-eiei/static/css/2.2452830e.chunk.css"
+  },
+  {
+    "revision": "05b300148ca1feacca60",
+    "url": "/dialog-eiei/static/css/main.68260a9f.chunk.css"
+  },
+  {
+    "revision": "109bc450ff9cc3e67023",
+    "url": "/dialog-eiei/static/js/2.d4f6c9dc.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/dialog-eiei/static/js/2.d4f6c9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b300148ca1feacca60",
+    "url": "/dialog-eiei/static/js/main.503be476.chunk.js"
+  },
+  {
+    "revision": "ea9a5fe729e3463e3429",
+    "url": "/dialog-eiei/static/js/runtime-main.aae7df9e.js"
+  },
+  {
+    "revision": "1f6d643fbc008e0df97c9f01290dbaf1",
+    "url": "/dialog-eiei/static/media/MitrRegular.1f6d643f.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/dialog-eiei/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
